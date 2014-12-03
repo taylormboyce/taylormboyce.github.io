2ac46c3058ab92d3398808bdd8f34cd6d1e78828
taylormboyce <taylormboyce@gmail.com>
2014-12-03 00:13:36 -0500

    personal end

--- contact/contactform.php
+++ contact/contactform.php
@@ -16,7 +16,7 @@ $formproc->EnableCaptcha($sim_captcha);
 
 //1. Add your email address here.
 //You can add more than one receipients.
-$formproc->AddRecipient('tests@html-form-guide.com'); //<<---Put your email address here
+$formproc->AddRecipient('tboyce@ufl.edu'); //<<---Put your email address here
 
 
 //2. For better security. Get a random tring from this link: http://tinyurl.com/randstr
